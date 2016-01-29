19ac7f32bd82d4b481e68cf0853d31fd424db333
Julien Richard-Foy <julien@richard-foy.fr>
2016-01-29 11:21:02 +0100

    experiment with another design (probably less error prone)

--- src/main/scala/julienrf/json/derived/package.scala
+++ src/main/scala/julienrf/json/derived/package.scala
@@ -6,4 +6,6 @@ package object derived {
 
   def reads[A](implicit derivedReads: DerivedReads[A]): Reads[A] = derivedReads
 
+  def reads2[A](implicit derivedReads2: DerivedReads2[A]): Reads[A] = derivedReads2.reads
+
 }
